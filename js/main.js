9e0d363337900c3cb10698fa5e778fd49995a8d2
AntonioDattolo <antonio19794.a@gmail.com>
2024-04-17 17:25:26 +0200

    Fixed decimal number

--- js/main.js
+++ js/main.js
@@ -8,16 +8,14 @@ function elaborated(){
     const minor = biglietto / 100 * 20
     const senior = biglietto / 100 * 40
     if (hyears >=18 && hyears < 65){
-        let biglietto = hkm * pkm 
-        document.getElementById("titi").innerHTML ="il prezzo è" + biglietto
+        let bigliett = hkm * pkm 
+        document.getElementById("titi").innerHTML =`${biglietto.toFixed(1)} €`
     }else if (hyears < 18){
-    
         let bigliettoMinor = biglietto - minor
-        document.getElementById("titi").innerHTML ="il prezzo è" + bigliettoMinor
+        document.getElementById("titi").innerHTML =`${bigliettoMinor.toFixed(1)} €`
     }else if (hyears >= 65){
-    
         let bigliettoSenior = biglietto - senior
-        document.getElementById("titi").innerHTML ="il prezzo è " + bigliettoSenior
+        document.getElementById("titi").innerHTML =`${bigliettoSenior.toFixed(1)} €`
     }
     
 }
