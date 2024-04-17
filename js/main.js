056579dbc20e9cb89003fdd7f4f73ad76b51659d
AntonioDattolo <antonio19794.a@gmail.com>
2024-04-17 17:05:08 +0200

    Train tickets fix

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 
 
 function elaborated(){
+    const pkm = 0.20
     let hkm = document.getElementById("how-km").value;
     var hyears = document.getElementById("hm-years").value;
-    const biglietto = hkm * hyears
-    document.getElementById("titi").innerHTML ="il prezzo è" + biglietto
-    alert(biglietto)
+    let biglietto = hkm * pkm
+    const minor = biglietto / 100 * 20
+    const senior = biglietto / 100 * 40
+    if (hyears >=18 && hyears < 65){
+        let biglietto = hkm * pkm 
+        document.getElementById("titi").innerHTML ="il prezzo è" + biglietto
+    }else if (hyears < 18){
+    
+        let bigliettoMinor = biglietto - minor
+        document.getElementById("titi").innerHTML ="il prezzo è" + bigliettoMinor
+    }else if (hyears >= 65){
+    
+        let bigliettoSenior = biglietto - senior
+        document.getElementById("titi").innerHTML ="il prezzo è " + bigliettoSenior
+    }
+    
 }
-
